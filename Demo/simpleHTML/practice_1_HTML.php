8a9bcdbb6960f759a3b9637bbb266e8223f56571
ivan sheh <ivan sheh>
2016-04-30 17:18:42 +1000

    ivan_HTML_frame_practice

---
+++ Demo/simpleHTML/practice_1_HTML.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+
+	<h1 align="center">HTML Page Structure</h1>
+
+<div id="container">
+
+	<div id="header"><p>Header</p></div>
+	<div id="nav"><p>Nav</p></div>
+	<div id="middle">
+		<div id="sidebar"><p>Sidebar</p></div>
+		<div id="main"><p>Main</p>
+			<div id="slider"><p>Slider</p></div>
+			<div id="tools"><p>Tools</p></div>
+			<div id="contents"><p>Contents</p></div>
+		</div>
+	</div>
+	<div id="footer"><p>Footer</p></div>
+</div>
+
+</body>
+</html>
