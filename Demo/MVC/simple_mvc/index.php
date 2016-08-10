c302727847b18f4640ccd3bc346249611dedcc48
YJ <yongjiang.zhang@nothingbutweb.com.au>
2016-08-10 12:01:17 +1000

    Add mvc demo

---
+++ Demo/MVC/simple_mvc/index.php
@@ -0,0 +1,14 @@
+<?php
+include_once( "Model.php" );
+include_once( "View.php" );
+include_once( "Controller.php" );
+
+$model = new Model();
+$controller = new Controller($model);
+$view = new View($controller, $model);
+
+if (isset($_GET['action']) && !empty($_GET['action'])) {
+    $controller->{$_GET['action']}();
+}
+
+echo $view->output();
